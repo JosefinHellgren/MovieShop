88384d89020649a2b1b316d297a969153c0c6ce4
josefinhellgren <josefin.hellgren@gmail.com>
2023-04-21 09:59:44 +0200

    Added Payment component and connected with button from MainPage

---
+++ src/compontents/Payment.jsx
@@ -0,0 +1,20 @@
+
+
+
+const Payment = (props) =>{
+
+
+    const movie = props.movie;
+
+
+    return(
+
+        <div>
+         <h2>{movie.title}</h2>  
+
+
+        </div>
+    )
+}
+
+export default Payment
