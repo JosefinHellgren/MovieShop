e6e2ec0910116373bde9faab4353b25e47e90c40
ciliaence <cilia.ence@telia.com>
2023-04-23 13:36:30 +0200

    added top rated and now playing movies, as well as a MovieSlider.jsx

--- src/compontents/MainPage.jsx
+++ src/compontents/MainPage.jsx
@@ -1,59 +1,38 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Slider from "react-slick";
-import './mainpage.css'
+import MovieSlider from "./MovieSlider";
+import "./mainpage.css";
 
 const MainPage = (props) => {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  const handleMovieClick = (movie) => {
+    props.setMovie(movie);
+    navigate("/movieinfo/");
+  };
 
-    const [popularMovies, setpopularMovies] = useState([]);
-    const apiKey = "305f99214975faee28a0f129881c6ec9";
-    const imgUrlStart = "https://image.tmdb.org/t/p/w185";
+  const handleButtonClick = (movie) => {
+    props.setMovie(movie);
+    navigate("/payment/");
+  };
 
-    //fetch movies
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`)
-            .then((response) => response.json())
-            .then((popularMovies) => {
-                console.log(popularMovies.results);
-                setpopularMovies(popularMovies.results)
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+  return (
+    <div className="mainpage_container">
+      <section className="popular_movies_section">
+        <h4>Popular Movies</h4>
+        <MovieSlider query="popular" setMovie={handleMovieClick} handleButtonClick={handleButtonClick} />
+      </section>
+      <section className="toprated_movies_section">
+        <h4>Top Rated Movies On IMDb</h4>
+        <MovieSlider query="top_rated" setMovie={handleMovieClick} handleButtonClick={handleButtonClick} />
+      </section>
+      <section className="now_playing_section">
+        <h4>In Theaters Now</h4>
+        <MovieSlider query="now_playing" setMovie={handleMovieClick} handleButtonClick={handleButtonClick} />
+      </section>
+      
+      
+    </div>
+  );
+};
 
-
-    }, [])
-
-    const handleMovieClick = (movie) => {
-        props.setMovie(movie);
-        navigate("/movieinfo/")
-    }
-    
-    const handleButtonClick = (movie) =>{
-        props.setMovie(movie);
-        navigate("/payment/")
-    }
-
-
-    return (
-        <div className="main_container">
-            <section className="popular_movies_section">
-                <h4>Popular Movies</h4>
-                <Slider className="slick-slider" slidesToShow={12}>
-                    {popularMovies && popularMovies.map((movie, index) => (
-                        <div key={index} className="popular_movies_container" >
-                            <img className="movie_poster" src={imgUrlStart + movie.poster_path} onClick={() => handleMovieClick(movie)} />
-                            <button className="payment_button" onClick={()=> handleButtonClick(movie)}>199:- Buy </button>
-                            <h6>{movie.title}</h6>
-                        </div>
-                    ))}
-                </Slider>
-            </section>
-            <section className="toprated_movies_section"></section>
-        </div>
-    )
-}
-
-export default MainPage
+export default MainPage;
