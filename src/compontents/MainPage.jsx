4d2810ae0119b75c089b2bcdfc783579215dfbe9
ciliaence <cilia.ence@telia.com>
2023-05-02 11:00:24 +0200

    added comments about redux

--- src/compontents/MainPage.jsx
+++ src/compontents/MainPage.jsx
@@ -10,20 +10,31 @@ import { useEffect } from "react";
 const MainPage = (props) => {
   let navigate = useNavigate();
 
+  //this line of code and the import of useDispatch is
+  // needed to save and clear selectedmovie to redux
   let dispatch = useDispatch();
 
   const handleMovieClick = (movie) => {
     props.setMovie(movie);
+
+    //this is what sets the selectedmovie to redux
     dispatch(selectActions.selectMovie(movie)); 
+
     navigate("/movieinfo/");
   };
 
   const handleButtonClick = (movie) => {
     props.setMovie(movie);
+
+    //this is what sets the selectedmovie to redux
     dispatch(selectActions.selectMovie(movie));
+    
     navigate("/payment/");
   };
 
+  //this useeffect clears the movie from redux 
+  //when the pathname no longer is movieinfo or payment
+  // meaning when the user clicks the "back-button"
   useEffect(() => {
     if (location.pathname !== "/movieinfo/" || location.pathname !== "/payment/" ) {
       dispatch(selectActions.clearMovie());
