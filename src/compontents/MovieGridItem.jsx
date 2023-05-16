01b1728dbb2f283697f38f5914207457e4bfb5a7
josefinhellgren <josefin.hellgren@gmail.com>
2023-05-16 10:09:40 +0200

    css moviegriditem och mainpge index

--- src/compontents/MovieGridItem.jsx
+++ src/compontents/MovieGridItem.jsx
@@ -5,7 +5,7 @@ const MovieGridItem = ({ movie, handleMovieClick, useBackDrop }) => {
 
     const imgUrlStart = "https://image.tmdb.org/t/p/original";
     const imageSource = useBackDrop ? movie.backdrop_path : movie.poster_path;
-    const movieStyle = useBackDrop ? { width: '100%', paddingTop: "50px" } : {};
+    const movieStyle = useBackDrop ? { width: '100%', paddingTop: "80px" } : {};
 
     return (
 
