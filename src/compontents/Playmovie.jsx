3f95aa13e2331b274579c71d335d753e06a074ff
Jonatan Grön <jonatan.gron@hotmail.se>
2023-05-21 23:19:45 +0200

    navigation backfrom playing movie

--- src/compontents/Playmovie.jsx
+++ src/compontents/Playmovie.jsx
@@ -1,23 +1,47 @@
 import React, {useRef, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 
 import './playmovie.css'
 import video from '../videos/movie.mp4'
 
 
+
 function Playmovie() {
 
 
- 
+
+  let navigate = useNavigate();
+
+  
+
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+
+
+  
+  //Går till fullscreen när man dubbelklickar på video
+  const handleFullscreenChange = () => {
+    setIsFullscreen((prevState) => !prevState);
+  };
 
 
 
 
     return (
       <div className="video-container">
-        <video className="video" controls autoPlay>
+        
+        <video className="video" controls autoPlay fullscreen playsInline onDoubleClick={handleFullscreenChange}>
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+       
+        <div className="overlay-text" onClick={handleGoBack}>
+        ← Click to Go Back
+      </div>
+
       </div>
     );
   }
