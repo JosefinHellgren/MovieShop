675c5c0d923715db51c6f053db29cd62103a2292
Jonatan Grön <jonatan.gron@hotmail.se>
2023-05-23 10:04:41 +0200

    new movie

--- src/compontents/Playmovie.jsx
+++ src/compontents/Playmovie.jsx
@@ -2,7 +2,7 @@ import React, {useRef, useEffect, useState} from 'react';
 import { useNavigate } from "react-router-dom";
 
 import './playmovie.css'
-import video from '../videos/movie.mp4'
+import video from '../videos/newmovie.mp4'
 
 
 
